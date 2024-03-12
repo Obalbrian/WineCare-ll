d867a49975474aa392280a7379a42d2e85c63621
SofiaP2002 <sofiaisabelperezhernandez@gmail.com>
2024-03-12 15:14:08 -0300

    menu

--- frontend/src/Componentes/Menu.js
+++ frontend/src/Componentes/Menu.js
@@ -3,12 +3,15 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import ImportarLogo from './ImportarLogo';
-import {Switch} from 'antd';
+
 import {useState} from 'react';
 
 
 export const Menu= () =>{
-    const [enabled, setEnabled] = useState(false)
+    const[toggleButton,setToggleButton]=useState(false);
+    const handleClick=()=>{
+        setToggleButton(!toggleButton)
+    }
     
 
   return (
@@ -25,13 +28,13 @@ export const Menu= () =>{
       
       
       <Nav className="ms-auto"  >
-       
-            
-      <Switch/>
-              
-         
-        
-
+        <div className='place'>
+         <div  onClick={handleClick} className='toggle'>
+            {toggleButton?<div className='toggle-left'></div>:
+            <div className='toggle-right'></div>
+            }
+            </div>
+        </div>
         </Nav >
         </Navbar.Collapse>
         </Container>
