08c44dd32f921eb9877c40292dc60205bf0c970a
SofiaP2002 <sofiaisabelperezhernandez@gmail.com>
2024-03-12 14:05:10 -0300

    menu

--- frontend/src/Componentes/Menu.js
+++ frontend/src/Componentes/Menu.js
@@ -1,15 +1,40 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import ImportarLogo from './ImportarLogo';
+import {Switch} from 'antd';
+import {useState} from 'react';
 
 
 export const Menu= () =>{
+    const [enabled, setEnabled] = useState(false)
+    
+
   return (
     <>
       <Navbar className='navBg' bg="navBg">
+      <Container className="navBg"  >
+      
+        <ImportarLogo/>{' '}WINE CARE
+      
+        <Navbar.Toggle  aria-controls="basic-navbar-nav" />
+
+           
+      <Navbar.Collapse id="basic-navbar-nav" className="navBg" > 
+      
+      
+      <Nav className="ms-auto"  >
+       
+            
+      <Switch/>
+              
+         
         
 
-        <ImportarLogo/>{' '}WINE CARE
+        </Nav >
+        </Navbar.Collapse>
+        </Container>
       </Navbar>
       
       
