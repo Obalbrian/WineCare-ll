0515afe183811a42009f2b7c0fe081fb017b8b73
Lan Yang Villavicencio <leelanzing@hotmail.com>
2024-03-19 00:38:56 -0300

    carpetas modelo vista controlador para backend

--- backend/App.js
+++ backend/App.js
@@ -3,8 +3,14 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(cors())
+app.use(express.json())
+
+
 const PORT = 4000;
 
 app.listen(PORT, ()=> {
     console.log('SERVIDOR FUNCIONANDO EN EL PUERTO', PORT);
 });
+
+module.exports = app
