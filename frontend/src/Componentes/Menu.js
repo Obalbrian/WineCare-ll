eae880c8359dfd3702e5313d6b49f6f796f2ef06
SofiaP2002 <sofiaisabelperezhernandez@gmail.com>
2024-03-19 13:54:25 -0300

    navbar

--- frontend/src/Componentes/Menu.js
+++ frontend/src/Componentes/Menu.js
@@ -3,13 +3,18 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import ImportarLogo from './ImportarLogo';
+import campana from '../img/campana.jpg';
+import usuario from '../img/usuario.jpg';
+import configuracion from '../img/configuracion.jpg'
+import { Button } from 'primereact/button';
+        
 
 
 import {useState} from 'react';
 
 
 export const Menu= () =>{
-    
+  
     
 
   return (
@@ -26,16 +31,23 @@ export const Menu= () =>{
       
       
       <Nav className="ms-auto"  >
-        
 
-        <div className='search-box'>
-          <input type='text' placeholder='Search'/>
-          
-        </div>
+      
+      
         
+      <div className='search-box'>
+          <input type='text' placeholder='Search'/>
+      
+        
+        </div >
         
         
         </Nav >
+
+
+        <img src={campana} alt="" width="45" height="50" className='toggle-icon'/>
+        <img src={usuario} alt="" width="40" height="65"className='toggle-icon'/>
+        <img src={configuracion} alt="" width="42" height="50"className='toggle-icon'/>
         </Navbar.Collapse>
         </Container>
       </Navbar>
